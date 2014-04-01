1e473e921884047707fcc4f569ba1e50ec10030b
Shai Yallin <shai.yallin@gmail.com>
2014-04-01 22:34:31 +0300

    Fib kata

--- src/test/scala/com/shaiyallin/FibonacciTest.scala
+++ src/test/scala/com/shaiyallin/FibonacciTest.scala
@@ -1,7 +1,6 @@
 package com.shaiyallin
 
 import org.specs2.mutable.Specification
-import scala.annotation.tailrec
 
 /**
  * @author shaiyallin
@@ -9,57 +8,6 @@ import scala.annotation.tailrec
  */
 
 class FibonacciTest extends Specification {
-
-  object Fib {
-
-    def fibRecursive(n: Int): Int =
-      if (n < 0)
-        throw new IllegalArgumentException
-      else if (n < 2)
-        1
-      else
-        fibRecursive(n - 2) + fibRecursive(n - 1)
-
-    def memoFib(n: Int): Int = {
-      import scala.collection.mutable
-      val memo = mutable.Map.empty[Int, Int]
-
-      def fib(n: Int) = memo.getOrElseUpdate(n,
-        if (n < 0)
-          throw new IllegalArgumentException
-        else if (n < 2)
-          1
-        else
-          fibRecursive(n - 2) + fibRecursive(n - 1))
-
-      fib(n)
-    }
-
-    def fibTailRec(n: Int): Int = {
-
-      @tailrec
-      def fib(n: Int, cur: Int, sum: Int): Int = {
-        if (n == 0)
-          sum
-        else
-          fib(n - 1, sum, sum + cur)
-      }
-
-      if (n < 0)
-        throw new IllegalArgumentException
-      else
-        fib(n, 0, 1)
-    }
-
-    def streamingFib(n: Int): Int = {
-      if (n < 0)
-        throw new IllegalArgumentException
-
-      lazy val fibs: Stream[Int] = 1 #:: fibs.scanLeft(1)(_ + _)
-
-      fibs.take(n + 1).last
-    }
-  }
 
   import Fib.{memoFib => fib}
 
